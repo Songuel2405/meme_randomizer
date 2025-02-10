5420ca5c115fd0c5cb2cc0a5139e70211e804421
itiszara <za26ra@outlook.de>
2025-02-10 21:56:17 +0100

    Startfenster umbenannt - kleines weißes Kästchen entfernt - Kategorie Meerestiere entfernt da leer (Wildtiere behalten für Beispiel Fehlermeldung) - kommentiert

--- meme_randomizer.py
+++ meme_randomizer.py
@@ -1,10 +1,10 @@
 # Imports
-import random                       # für die zufällige Ausgabe der Memes
-import os                           # Interaktion mit dem Betriebssystem, in diesem Fall Zugriff auf die Dateien der Memes
-import tkinter as tk 
-from tkinter import filedialog, colorchooser, font# GUI für die grafische Benutzeroberfläche
-from PIL import Image, ImageTk, ImageOps      # Bibliothek für Bildverarbeitung; Image=Klasse für Bilder öffnen, bearbeiten und speichern; ImageTk=Schnittstelle zu Tkinter
-from tkinter import messagebox      # ermöglicht Pop-up Nachrichten
+import random                                       # für die zufällige Ausgabe der Memes
+import os                                           # Interaktion mit dem Betriebssystem, in diesem Fall Zugriff auf die Dateien der Memes
+import tkinter as tk                                # GUI Tool - graphical user interface
+from tkinter import filedialog, colorchooser, font  # Dateien öffnen+speichern - Farbwähler - Schriftart
+from tkinter import messagebox                      # ermöglicht Pop-up Nachrichten
+from PIL import Image, ImageTk, ImageOps            # Bibliothek für Bildverarbeitung; Image=Klasse für Bilder öffnen, bearbeiten und speichern; ImageTk=Schnittstelle zu Tkinter
 import emoji
 
 # Dictionary mit Ordnern mit Memes nach Kategorie
@@ -17,7 +17,6 @@ CATEGORIES = {
      "Elefant": r"memes\Tiere\Elefant",
      "Maus": r"memes\Tiere\Maus",
      "Wildtiere": r"memes\Tiere\Wildtiere",
-     "Meerestiere":r"memes\Tiere\Meerestiere",
      "Vögel": r"memes\Tiere\Vögel"},
      "Emotionen": 
      {"Glücklich": r"memes\Emotionen\Glücklich",
@@ -31,93 +30,101 @@ CATEGORIES = {
 
 # Meme-Randomizer starten
 def start_meme_randomizer():
-    start_window.configure(bg="grey25")
-    start_window.destroy()
+    start_window.configure(bg="grey25")                 # Randomizer Fenster in grau konfigurieren
+    start_window.destroy()                              # Startfenster schließen
 
-    global root                                         
-    root = tk.Tk()
-    root.geometry("800x600")                                      # Hauptfenster
+    global root                                         # global damit root auch außerhalb funktioniert
+    root = tk.Tk()                                      # erstellt das Hauptfenster des Randomizers
+    root.geometry("800x600")                            # Startgröße des Fensters
     root.title("Meme Randomizer")                       # gibt dem Fenster einen Titel
     root.configure(bg="grey25")                         # grauer Hintergrund
 
+    # Kategorien anzeigen
     def show_categories():
         for widget in frame.winfo_children():
-            widget.destroy()                            # entfernt die bestehenden Widgets
-        meme_label.config(image="", text="")            # löscht Bild und Text im Label
+            widget.destroy()                                # entfernt die bestehenden Widgets im frame, doppelte Buttons vermeiden
+        meme_label.config(image="", text="",bg="grey25")    # löscht Bild und Text im Label
 
-        for category in CATEGORIES:
-            if isinstance(CATEGORIES[category], dict):
+        for category in CATEGORIES:                         # iteriert durch alle Kategorien
+            if isinstance(CATEGORIES[category], dict):      # wenn eine Kategorie Unterkategorien hat, wird def show_subcategories aufgerufen
                 btn = tk.Button(frame, text=category, font=("Alasassy Caps", 12), fg="white", bg="grey25", command=lambda c=category: show_subcategories(c))
-            else:
+            else:                                           # ansonsten wird def show_random_meme aufgerufen
                 btn = tk.Button(frame, text=category, font=("Alasassy Caps", 12), fg="white", bg="grey25", command=lambda c=category: show_random_meme(c, None))
-            btn.pack(side=tk.LEFT, padx=10, pady=5)     # fügt die Buttons ins frame ein
-
+            btn.pack(side=tk.LEFT, padx=10, pady=5)         # fügt die Buttons ins frame ein
+    
+    # Unterkategorien anzeigen
     def show_subcategories(category):
-        for widget in frame.winfo_children():
-            widget.destroy()                            # entfernt bestehende Widgets
-        for subcategory in CATEGORIES[category]:        # iteriert durch die Unterkategorrien
-            btn = tk.Button(frame, text=subcategory, font=("Alasassy Caps", 12), fg= "white", bg="gray25", command=lambda c=category, s=subcategory: show_random_meme(c, s))
-            btn.pack(side=tk.LEFT, padx=10, pady=5)     # bettet den Button für die Unterkategorien ein
+        for widget in frame.winfo_children():           
+            widget.destroy()                                                                                # entfernt bestehende Buttons
+        for subcategory in CATEGORIES[category]:                                                            # iteriert durch die Unterkategorien
+            btn = tk.Button(frame, text=subcategory, font=("Alasassy Caps", 12), fg= "white", bg="gray25",  # Buttons für die Unterkategorien erstellen, 
+            command=lambda c=category, s=subcategory: show_random_meme(c, s))                               # die bei Klick ein random meme aufrufen
+            btn.pack(side=tk.LEFT, padx=10, pady=5)                                                         # bettet den Button für die Unterkategorien ein
 
         back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12), fg="white", bg="grey25", command= show_categories)  # definiert den Zurück-Button
-        back_btn.pack(side=tk.LEFT, padx=10, pady=5)   # bettet den Button im Tkinter-Fenster ein, mit automatischer Skallierung des Buttons; Abstand zu anderen Objekten 10 Pixel horizontal und 5 Pixel vertikal
+        back_btn.pack(side=tk.LEFT, padx=10, pady=5)   # bettet den Button ein, mit automatischer Skallierung des Buttons; Abstand zu anderen Objekten 10 Pixel horizontal und 5 Pixel vertikal
     
     # Frame-Widget mit Scroll-Leiste erstellen
-    canvas = tk.Canvas(root)
-    canvas.configure(bg="grey25")
-    scrollbar = tk.Scrollbar(root,orient="vertical", command=canvas.yview)
-    frame = tk.Frame(canvas, background= "grey25")    # erstellt ein Frame-Widget im Hauptfenster mit grauem Hintergrund, das als Container für die Buttons dient
-    frame.pack()                        # bettet das Frame in das Fenster ein
-    frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
-    canvas.create_window((0, 0), window=frame, anchor="nw")
-    canvas.configure(yscrollcommand=scrollbar.set)
-    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)            # bettet die Scroll-Leiste ein
+    canvas = tk.Canvas(root)                                                # "Leinwand" erstellen
+    canvas.configure(bg="grey25")                                           # in grau
+    scrollbar = tk.Scrollbar(root,orient="vertical", command=canvas.yview)  # erstellt eine vertikale Scroll-Leiste im Hauptfenster
+    frame = tk.Frame(canvas, background= "grey25")                          # erstellt ein Frame-Widget im Hauptfenster mit grauem Hintergrund, das als Container für die Buttons dient
+    frame.pack()                                                            # bettet das Frame in das Canvas ein
+    frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))  # konfiguriert die Scroll-Leiste
+    canvas.create_window((0, 0), window=frame, anchor="nw")                 # Position (nord-west) des Frames als Fenster im Canvas
+    canvas.configure(yscrollcommand=scrollbar.set)                          # verknüpft Scrollbar mit Canvas
+    canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)                    # platziert Canvas und lässt ihn den verfügbaren Platz einnehmen
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)                                # bettet die Scroll-Leiste rechts ein
 
-    # Suchfeld zur GUI hinzufügen
-    search_var = tk.StringVar()                             # Variable für den Suchbegriff
-
-    search_entry = tk.Entry(root, textvariable=search_var, font=("Alasassy Caps", 12))  # Eingabefeld im Hauptfenster root erstellen und Ergebnis in Variable speichern
-    search_entry.pack(pady=10)  # Abstand zur Leserlichkeit                                   # und einbetten
-
-    search_button = tk.Button(root, text="🔍 Suchen", font=("Alasassy Caps", 12), 
-                          fg="white", bg="grey25",  command=lambda: search_memes(search_var.get()))   # Suchbutton erstellen
-    search_button.pack() # und einbetten
-
+    # Suchfeld hinzufügen
+    search_var = tk.StringVar()                                                                         # Variable für den Suchbegriff
+    search_entry = tk.Entry(root, textvariable=search_var, font=("Alasassy Caps", 12))                  # Eingabefeld im Hauptfenster root erstellen und Ergebnis in Variable speichern
+    search_entry.pack(pady=10)  # Abstand zur Leserlichkeit                                             # und einbetten
+    search_button = tk.Button(root, text="🔍 Suchen", font=("Alasassy Caps", 12),                      
+                          fg="white", bg="grey25",  command=lambda: search_memes(search_var.get()))     # Suchbutton erstellen der def search_memes aufruft
+    search_button.pack()                                                                                # und einbetten
+    
     # Suchfunktion
     def search_memes(query):
         query = query.lower()   # alles klein schreiben um Suche zu vereinfachen
         results = []            # Leere Liste für die Dateipfade der Suchergebnisse
 
-        for category, value in CATEGORIES.items():                              # iteriert durch alle Kategorien und Unterkategorien
-            if isinstance(value, dict):                                         # falls es Unterkategorien gibt (value=dict)
-                for subcategory, path in value.items():                         # iteriert durch alle Bilder der Unterkategorie
+        for category, value in CATEGORIES.items():                              # iteriert durch alle Kategorien
+            if isinstance(value, dict):                                         # falls es Unterkategorien gibt
+                for subcategory, path in value.items():                         # iteriert durch alle Unterkategorien
                     if os.path.exists(path):                                    # prüft ob der Ordner existiert
                         for filename in os.listdir(path):                       # geht alle Dateien im Ordner durch und sucht nach Übereinstimmungen
                             if query in filename.lower():                       # falls der Dateiname den Suchbegriff enthält
                                 results.append(os.path.join(path, filename))    # wird der Pfad der Liste results hinzugefügt
             else:                                                               
                     if os.path.exists(value):                                   # falls es keine Unterkategorie gibt
-                        for filename in os.listdir(value):
-                            if query in filename.lower():
-                                results.append(os.path.join(value, filename))
+                        for filename in os.listdir(value):                      # geht alle Dateien im Ordner durch
+                            if query in filename.lower():                       # falls der Dateiname den Suchbegriff enthält
+                                results.append(os.path.join(value, filename))   # wird der Pfad der Liste results hinzugefügt
 
-            if not results:                                                         # falls nichts gefunden wurde
-                meme_label.config(text="Keine Ergebnisse gefunden", image="")
+            if not results:                                                     # falls nichts gefunden wurde
+                meme_label.config(text="Keine Ergebnisse gefunden",             # wird diese Meldung angezeigt
+                font=("Alasassy Caps", 12), fg="white", image="")                
                 return
     
-        for widget in frame.winfo_children():                                   # löscht alle bisherigen Buttons im frame, damit nur die neuen Suchergebnisse angezeigt werden
-            widget.destroy()
+        for widget in frame.winfo_children():                                   
+            widget.destroy()                                                    # löscht alle bisherigen Buttons im frame, damit nur die neuen Suchergebnisse angezeigt werden
 
         for meme_path in results:                                               # iteriert durch alle gefundenen Ergebnisse
-            btn = tk.Button(frame, text=os.path.basename(meme_path), font=("Alasassy Caps", 12), fg="white", bg="grey25",           # und erstellt einen Button der dann bei Klick das jeweilige Meme anzeigt
-                        command=lambda path=meme_path: show_meme(path))
+            btn = tk.Button(frame, text=os.path.basename(meme_path),
+            font=("Alasassy Caps", 12), fg="white", bg="grey25",                # und erstellt einen Button der dann bei Klick das jeweilige Meme anzeigt
+            command=lambda path=meme_path: show_meme(path))
             btn.pack()                                                          # bettet die Buttons ins Fenster ein
-    
+       
+        back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12),
+        fg="white", bg="grey25", command= show_categories)                      # definiert den Zurück-Button
+        back_btn.pack(side=tk.LEFT, padx=10, pady=5)                            # bettet den Button ein
+
     # Meme öffnen für die Suchfunktion
     def show_meme(meme_path):
-        if not os.path.exists(meme_path): # falls die Datei nicht mehr exestiert
-            meme_label.config(text="Fehler: Datei nicht gefunden", image="")
+        if not os.path.exists(meme_path):                                       # falls die Datei nicht mehr exestiert
+            meme_label.config(text="Fehler: Datei nicht gefunden",              # wird dieser Fehler angezeigt
+            font=("Alasassy Caps", 12), fg="white", image="")    
             return
     
         img = Image.open(meme_path)     # öffnet das Bild
@@ -137,7 +144,7 @@ def start_meme_randomizer():
         memes = os.listdir(folder)                      # listet alle Dateien des gewählten Ordners auf
     
         if not memes:                                   # falls kein Meme gefunden wurde 
-            meme_label.config(text="Keine Memes gefunden", image="")
+            meme_label.config(text="Keine Memes gefunden", font=("Alasassy Caps", 12), fg="white", image="")
             return
     
         random_meme = random.choice(memes)              # wählt zufällig ein Meme aus
@@ -153,10 +160,12 @@ def start_meme_randomizer():
         for widget in frame.winfo_children():
             widget.destroy()                            # vorherige Buttons entfernen
 
-        back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12), fg="white", bg="grey25", command= show_categories)  # definiert den Zurück-Button
+        back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12), 
+        fg="white", bg="grey25", command= show_categories)  # definiert den Zurück-Button
         back_btn.pack(side=tk.LEFT, padx=10, pady=5)   # bettet den Button im Tkinter-Fenster ein, mit automatischer Skallierung des Buttons; Abstand zu anderen Objekten 10 Pixel horizontal und 5 Pixel vertikal
 
-    back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12), fg="white", bg="grey25", command=show_categories)   # erstellt den Zurück-Button
+    back_btn = tk.Button(frame, text="← Zurück", font=("Alasassy Caps", 12), 
+    fg="white", bg="grey25", command=show_categories)   # erstellt den Zurück-Button
     back_btn.pack()
     # Label für das Bild
     meme_label = tk.Label(root)         # Erstellung eines Label-Widgets im Hauptfenster, wo das Meme drauf plaziert wird
@@ -167,17 +176,14 @@ def start_meme_randomizer():
     root.mainloop()                     # startet die Tkinter-Anwendung, Fenster bleibt geöffnet und reagiert auf Benutzerinteraktionen
 
 # Startfenster erstellen
-start_window = tk.Tk()
-start_window.title("Meme-Startmenü")
-start_window.geometry("400x300")
-start_window.configure(bg="grey25")
-
-label = tk.Label(start_window, text="Meme Master",font=("Goudy Stout", 14,),fg="white", bg= "grey25")
-label.pack(pady=20)
-
-btn_randomizer = tk.Button(start_window, text="Meme Randomizer", font=("Alasassy Caps", 12), fg="white", bg="grey25",command=start_meme_randomizer)
-btn_randomizer.pack(pady=10)
-
+start_window = tk.Tk()                  # Startfenster erstellen
+start_window.title("Meme Master")       # Name des Fensters
+start_window.geometry("400x300")        # Größe
+start_window.configure(bg="grey25")     # Hintergrund
+label = tk.Label(start_window, text="Meme Master",font=("Goudy Stout", 14),fg="white", bg= "grey25")    # Überschrift
+label.pack(pady=20)                                                                                     # einbetten
+btn_randomizer = tk.Button(start_window, text="Meme Randomizer", font=("Alasassy Caps", 12), fg="white", bg="grey25",command=start_meme_randomizer) # Button Meme Randomizer erstellen
+btn_randomizer.pack(pady=10)                                                                                                                        # einbetten
 
 class MemeGenerator:
     def __init__(self, root):
@@ -395,14 +401,13 @@ class MemeGenerator:
         pass
     
 def start_meme_generator():
-    start_window.destroy()
-    root = tk.Tk()  # Erstelle das Tkinter-Hauptfenster
-    root.configure(bg="grey25")
-    app = MemeGenerator(root)  # Erstelle das Meme-Generator-Objekt
+    start_window.destroy()      # Startfenster schließen
+    root = tk.Tk()              # Erstelle das Hauptfenster des Generators
+    root.configure(bg="grey25") # in grau
+    app = MemeGenerator(root)   # Erstelle das Meme-Generator-Objekt
     root.mainloop()
 
-btn_generator = tk.Button(start_window, text="Meme Generator", font=("Alasassy Caps", 12),fg="white", bg="grey25", command=start_meme_generator)
-btn_generator.pack(pady=10)
-
+btn_generator = tk.Button(start_window, text="Meme Generator", font=("Alasassy Caps", 12),fg="white", bg="grey25", command=start_meme_generator)    # Button erstellen für Meme Generator
+btn_generator.pack(pady=10)                                                                                                                         # einbetten
 
 start_window.mainloop()
